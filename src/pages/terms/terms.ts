fb86397c885b30c7bca85dfbec8879d97b8f4945
AlexReiner <alex@ithomies.de>
2018-12-03 07:10:50 +0100

    Version 0.2 - enhancement
    Button für manuelle Standortsuche

--- src/pages/terms/terms.ts
+++ src/pages/terms/terms.ts
@@ -7,7 +7,7 @@ import { NavController, Platform } from 'ionic-angular';
 })
 export class TermsPage {
 
-  public version: string = '0.1';
+  public version: string = '0.2';
 
   constructor(public navCtrl: NavController, private platform: Platform) { }
 
